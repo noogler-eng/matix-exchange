697aaf5b4358164a9d7bd9e7071d829e949db51e
shared <poddarsharad460@gmail.com>
2024-08-07 11:33:17 +0530

    added

--- app/api/tokens/route.ts
+++ app/api/tokens/route.ts
@@ -1,16 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+// import { Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { supported_token } from "./supported_token";
+import { getAccount, getAssociatedTokenAddress } from "@solana/spl-token"
+import { Connection, PublicKey } from "@solana/web3.js";
+
+const connection = new Connection("https://api.mainnet-beta.solana.com")
 
 export async function GET(req: NextRequest){
     const { searchParams } = new URL(req.url);
-    const searchQuery = searchParams.get('address') || "";
+    const address = searchParams.get('address') || "";
+    // ata => associated token account
+    // pda => program derived address
     
-    const connection = new Connection("https://api.devnet.solana.com");
-    let txhash = await connection.requestAirdrop((searchQuery), 1e9);
-    let balance = await connection.getBalance(searchQuery);
-    const noOfSol = balance / LAMPORTS_PER_SOL;
+    const balance = await Promise.all(supported_token.map((token)=>{
+        return getAccountBalance(token, address);
+    }))
 
-    NextResponse.json({
-        balance: noOfSol * 80,
-    })
+
+}
+
+
+async function getAccountBalance(token: {
+    name: string,
+    mint: string
+}, address: string){
+
+    const ata = await getAssociatedTokenAddress(new PublicKey(token.mint), new PublicKey(address));
+    console.log('ata: ', ata);
+    const account = await getAccount(connection, ata);
+
+    
+
 }
---
+++ app/api/tokens/supported_token.ts
@@ -0,0 +1,9 @@
+export const supported_token = [
+    {
+        name: "USDC",
+        mint: "FSxJ85FXVsXSr51SeWf9ciJWTcRnqKFSmBgRDeL3KyWw"
+    },{
+        name: "USDT",
+        mint: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+    }
+]
